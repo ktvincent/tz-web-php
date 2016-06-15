e67f7f66d5cf2a24ff64ed9c17feb3c2e99da3f4
vincent.navales <vincent.navales@hotmail.fr>
2016-06-15 10:47:33 -0400

    create folders in models and added MediaProject.php (VO)

---
+++ models/Project/MediaProject/MediaProject.php
@@ -0,0 +1,40 @@
+<?php
+
+class MediaProject extends Project{
+  
+  private var $support;
+  private var $type;
+  
+  private var $differentTypes = array('video','son');
+  private var $differentSupports = array('numeric','cd','dvd');
+  
+  public function __construct($name,$startDate,$description,$endDate,$support,$type){
+    parent::_construct($name,$startDate,$description,$endDate);
+    $this->setSupport($support);
+    $this->setType($type);
+  }
+//Support methods  
+  public function setSupport($support){
+    if(in_array($support,$this->differentSupports)){
+      $this->support = $support;
+    }
+  }
+  
+  public function getSupport(){
+    return this->support;
+  }
+//Type methods  
+  public function setType($type){
+    if(in_array($type,$this->differentTypes)){
+      $this->type = $type;
+    }
+  }
+  
+  public function getType(){
+    return this->type;
+  }
+  
+  
+}
+
+?>
