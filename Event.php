238b01b803c3dfde9e395d3a01c09396d9705cae
lukas.brasseleur <lukas.brasseleur@gmail.com>
2016-06-16 07:18:09 -0400

    dao +class

---
+++ Event.php
@@ -0,0 +1,24 @@
+<?php
+public class Event{
+  private $location;
+  private $dateTime;
+
+  public function __construct($location, $dateTime){
+    $this->location = $location;
+    $this->dateTime = $dateTime;
+  }
+
+  public function getLocation(){
+    return $this->location;
+  }
+  public function getDateTime(){
+    return $this->dateTime;
+  }
+  public function setLocation($location){
+    return $this->location = $location;
+  }
+  public function setdateTime(){
+    return $this->dateTime = $dateTime;
+  }
+}
+?>
