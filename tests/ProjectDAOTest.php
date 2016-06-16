0811e7193710198f649b019b3766124e126c7355
simonvasseur <Simonvasseur91550@gmail.com>
2016-06-16 15:34:21 +0200

    change asserNull to assertEquals(0)

--- tests/ProjectDAOTest.php
+++ tests/ProjectDAOTest.php
@@ -4,10 +4,10 @@ require_once('../models/ProjectDAO.php');
 use PHPUnit\Framework\TestCase;
 
 class ProjectDAOTest extends PHPUnit_Framework_TestCase	{
-	public function testGetList()	{
+	public function testGetEmptyList()	{
 		$object = new ProjectDAO();
 		$test = $object::getList();
-		$this->assertNull($test);
+		$this->assertEquals(0,count($test));
 	}
 }	
 
