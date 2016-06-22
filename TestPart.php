8425fbe844893b8c4532b9837f4fa1488feaffa9
ludoviccarlu <ludoviccarlu@gmail.com>
2016-06-22 14:43:43 -0400

    Ajout de test

--- TestPart.php
+++ TestPart.php
@@ -1,34 +1,44 @@
 <?php
 //nicolas@karageusian.com
-require_once 'partClass.php';
-require_once 'partDAO.php';
+require_once 'PartClass.php';
+require_once 'PartDAO.php';
 
 use PHPUnit\Framework\TestCase;
 
 class TestPart extends PHPUnit_Framework_TestCase {
   
-  public function testnewPart() {
-    $part=new part(1,11,4,120,'2016-06-16');
+  public function testNewPart() {
+    $part=new PartClass(1,11,4,120,'2016-06-16');
     $this->assertNotEmpty($part);
   }
   
-
-  public function testListDao() {
-    $dao=new partDAO();
-   	$list=$dao->getListPart();
-    $this->assertNotNull($list);
-  }
-  public function testgetEmptyList() {
+  public function testGetEmptyList() {
+    $tab = array();
+    if($this->assertNotEmpty(PartDAO::getListPart())) {
+      return $tab;
+    } 
   //Doit return un tableau vide si oui
   }
   
   public function testCreatePart() {
-    $part = new Part();
-    $id = $dao->createPart($part);
+    $part=new part(1,11,4,120,'2016-06-16');
+    $id = PartDAO::createPart($part);
     $this->assertInternalType("int",$id);
   }
   
-  public function getById() {
-    $dao->getById
+  public function testUpdatePriceFromIdPart() {
+    $count = PartDAO::updatePriceFromIdPart(1,1);
+    $this->assertGreaterThanOrEquals(1,$count);
   }
+  
+  public function testDeletePartFromIdPart() {
+    $count = PartDAO::deletePartFromIdPart(1);
+    $this->assertGreaterThanOrEquals(1,$count);
+  }
+  
+  public function testDeletePartFromIdProject() {
+    $count = PartDAO::deletePartFromIdProject(1);
+    $this->assertGreaterThanOrEquals(1,$count);
+  }
+  
 ?>
